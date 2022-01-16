81824cbf2699339994f12182c84eb3bf2648558d
Shri Khare <khare@exploremetakinetic.com>
2022-01-16 16:41:28 -0500

    updated RecipeFlask's database input

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
-
+from socket import create_connection
+from flask import Flask, render_template, request, redirect,url_for,abort
+import sqlite3
+from sqlite3 import Error
 app = Flask(__name__)
 recipes = [
         {
@@ -9,10 +11,38 @@ recipes = [
         },
         {
             "title": "Hot Chicken Wings",
-            "image": "https://image.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg",
-            "link": "https://cookpad.com/us/recipes/347447-easy-sweet-sour-bbq-chicken"
+            "image": "https://image.freepik.com/free-photo/barbecue-chicken-wings-with-white-sesame_1339-98947.jpg",
+            "link": "https://cookpad.com/us/recipes/14731185-out-of-this-world-hot-wings"
         }
     ]
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+def execute_query(connection, query, optional = 0):
+    cursor = connection.cursor()
+    # print(query)
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+create_recipes_table = """
+CREATE TABLE IF NOT EXISTS recipes (
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  image BLOB NOT NULL,
+  link INTEGER NOT NULL
+);
+"""
+
 @app.route('/')
 def home():
     
@@ -22,5 +52,13 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/recipe/', methods=['POST', 'GET'])
+def create_recipe():
+    form = request.form
+    return redirect(url_for('home'))
+
 if __name__ == '__main__':
+  conn = create_connection('recipes.db')
+  
+  execute_query(conn,create_recipes_table)
   app.run(debug=True)
